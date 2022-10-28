be1f8e460df4437f5070128ff37e063cd9b3597f
unknown <nidhib.albiorix@gmail.com>
2022-10-28 11:55:03 +0530

    bcrypy issue fixed

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,21 +1,28 @@
-const bcrypt = require('bcrypt');
+import bcrypt from "bcrypt";
 
-const salt_round=18
-export function hassPassword(password:string):Promise<string>{
-    return new Promise<string>((resolve,reject)=>{
-        bcrypt.hash(password,salt_round,(err:Error, encrypted:string)=>{
-            if(err) return reject(err)
+const salt_round = 18;
+export function hassPassword(password: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    bcrypt.hash(
+      password,
+      salt_round,
+      (err: Error | undefined, encrypted: string) => {
+        if (err) return reject(err);
 
-            resolve(encrypted)
-        })
-    })
+        resolve(encrypted);
+      }
+    );
+  });
 }
 
-export function matchPassword(hash:string,password:string):Promise<boolean>{
-    return new Promise<boolean>((resolve,reject)=>{
-        bcrypt.compare(password,hash,(err:Error,same:boolean)=>{
-            if(err) return reject(err)
-            resolve(same)
-        })
-    })
+export function matchPassword(
+  hash: string,
+  password: string
+): Promise<boolean> {
+  return new Promise<boolean>((resolve, reject) => {
+    bcrypt.compare(password, hash, (err: Error | undefined, same: boolean) => {
+      if (err) return reject(err);
+      resolve(same);
+    });
+  });
 }
