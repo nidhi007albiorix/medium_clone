e0d39f30f0aa76a27f157e5ca14ddf7510e2d066
unknown <nidhib.albiorix@gmail.com>
2022-10-29 16:31:02 +0530

    cors issue

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ".medium-clone-frontend.pages.dev"
+    origin: "https://medium-clone-frontend.pages.dev/"
   })
 );
 app.get("/", (req: Request, res: Response) => {
