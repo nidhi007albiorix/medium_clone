bcc0cd8a3a845b35c4202186306220fd0d1c7764
unknown <nidhib.albiorix@gmail.com>
2022-10-29 16:25:28 +0530

    cors issue

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://fdbdeca5.medium-clone-frontend.pages.dev"
+    origin: ".medium-clone-frontend.pages.dev"
   })
 );
 app.get("/", (req: Request, res: Response) => {
